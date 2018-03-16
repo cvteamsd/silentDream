1455a153f2d40abd8378188ab68ae791950bfb01
houren wang <wanghourenrd@163.com>
2018-03-16 18:44:36 +0800

    Bringup Feature:refine code Task ID: Feature Highlight: Limitation: ToDo:

--- src/PluginBase.h
+++ src/PluginBase.h
@@ -19,7 +19,7 @@ private:
     DISALLOW_EVIL_CONSTRUCTORS(PluginBase);
 };
 
-static std::string str_tolower(std::string s) {
+static inline std::string str_tolower(std::string s) {
     std::transform(s.begin(), s.end(), s.begin(),
                    [](unsigned char c){ return std::tolower(c); }
                   );
